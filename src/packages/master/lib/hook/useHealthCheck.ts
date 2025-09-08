d381b60a7349ac0a07c29e7d493799fba5911ce2
Quan Nguyen Ba <quan.nguyenba@pionero.io>
2025-09-08 14:44:34 +0700

    feat: update package exports to improve performance and add health check functionality

--- src/packages/master/lib/hook/useHealthCheck.ts
+++ src/packages/master/lib/hook/useHealthCheck.ts
@@ -4,10 +4,18 @@ import { API_URLS } from '../constants/url'
 
 const useHealthCheck = () => {
   const httpClient = useHttpClient()
+
   useEffect(() => {
+    const shouldRunHealthCheck =
+      process.env.NEXT_PUBLIC_ENABLE_HEALTH_CHECK === 'true'
+
+    if (!shouldRunHealthCheck) {
+      return
+    }
+
     const triggerHealth = async () => {
       try {
-        await httpClient.get(API_URLS.HEALTH_API_URL)
+        await httpClient.get(API_URLS.HEALTH)
       } catch (error) {
         console.error('Health check failed:', error)
       }
